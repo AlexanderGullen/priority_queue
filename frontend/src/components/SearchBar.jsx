af4222e7d27fb016e52b329eb78184d348d7f411
AlexanderGullen <developer@alexmgullen.ca>
2025-02-10 08:00:12 -0500

    fix: updated searchbar functionality

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,6 +3,11 @@ import { useRef, useEffect } from 'react'
 export function SearchBar({tasks,setSearchResults}){
     const inputRef = useRef()
 
+    useEffect(() => {
+
+        setSearchResults(tasks)
+    },[tasks,setSearchResults])
+
     const handleSearchChange = (e) => {
         if (!e.target.value) return setSearchResults(tasks)
 
